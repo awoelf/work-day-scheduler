bbf61654de6efcc289af63fc321f1359d09eefce
awoelf <98348782+awoelf@users.noreply.github.com>
2022-10-16 13:10:21 -0500

    work day scheduler complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,23 +9,23 @@ function displayTime() {
 
 function createHours() {
   for (let i = 9; i < lastHour + 1; i++) {
-    scheduleContainer.addClass("").append(
+    scheduleContainer.append(
       $("<div>")
         .addClass("row my-1")
-        .attr("id", `hour${i}`)
+        .attr("id", `row-${i}`)
         .append(
           $("<div>")
-            .addClass("col-1 d-flex justify-content-center rounded-left hour-col")
+            .addClass(
+              "col-1 d-flex justify-content-center rounded-left hour-col"
+            )
             .append($("<p>").addClass("my-auto").attr("id", "time"))
         )
         .append(
           $("<div>")
-            .addClass(
-              "col-10 activity d-flex justify-content-start text-col"
-            )
+            .addClass("col-10 activity d-flex justify-content-start text-col")
             .append(
               $("<textarea>")
-                .attr("id", "activity")
+                .attr("id", `activity-${i}`)
                 .attr("rows", "2")
                 .attr("placeholder", "Add your work activity...")
                 .addClass("my-auto w-100")
@@ -33,8 +33,10 @@ function createHours() {
         )
         .append(
           $("<div>")
-            .addClass("col-1 d-flex justify-content-center btn btn-primary rounded-right save-btn")
-            .attr("id", "btn")
+            .addClass(
+              "col-1 d-flex justify-content-center btn btn-primary rounded-right save-btn"
+            )
+            .attr("id", `${i}`)
             .append(
               $("<span>")
                 .addClass("material-symbols-outlined my-auto")
@@ -42,20 +44,51 @@ function createHours() {
             )
         )
     );
-    $(`#hour${i}`).find("#time").text(moment().hour(i).format("h A"));
+    $(`#row-${i}`).find("#time").text(moment().hour(i).format("h A"));
+    textareaBg(i);
+    console.log(i);
   }
 }
 
 // Allows user to save activity to respective hour to local storage
-function saveActivity() {
+function saveActivity(hourId) {
+  localStorage.setItem(`row-${hourId}`, JSON.stringify($(`#activity-${hourId}`).val()));
+}
 
+function loadActivity() {
+  for (let i = 9; i < lastHour + 1; i++) {
+    if (localStorage.getItem(`row-${i}`)) {
+      $(`#activity-${i}`).val(localStorage.getItem(`row-${i}`).replace(/['"]+/g, ''));
+    }
+  }
 }
 
 // Changes the background color of the hour's textarea to reflect the past, present, and future status.
-function currentHour() {
-
+function textareaBg(hourId) {
+  if (hourId == moment().hour()) {
+    $(`#row-${hourId}`)
+      .children(".text-col")
+      .attr("style", "background-color: #d7c7b6");
+  } else if (hourId < moment().hour()) {
+    $(`#row-${hourId}`)
+      .children(".text-col")
+      .attr("style", "background-color: #9bc3c0");
+  } else {
+    $(`#row-${hourId}`)
+      .children(".text-col")
+      .attr("style", "background-color: #dad7b2");
+  }
 }
+
+
 
 displayTime();
 
 createHours();
+
+loadActivity();
+
+$(document).on("click", ".btn", function (event) {
+  event.preventDefault();
+  saveActivity(this.id);
+});
